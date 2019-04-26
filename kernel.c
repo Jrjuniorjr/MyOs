a24ccd2512ba262aef75530711be941a914535f3
Jrjuniorjr <mjuniorjrjr@gmail.com>
2019-04-25 23:38:39 -0300

    Tentativa de fazer o scrolling no kernel. Com erro.

--- kernel.c
+++ kernel.c
@@ -1,19 +1,20 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
- 
+
 /* Check if the compiler thinks you are targeting the wrong operating system. */
 #if defined(__linux__)
 #error "You are not using a cross-compiler, you will most certainly run into trouble"
 #endif
- 
+
 /* This tutorial will only work for the 32-bit ix86 targets. */
 #if !defined(__i386__)
 #error "This tutorial needs to be compiled with a ix86-elf compiler"
 #endif
- 
+
 /* Hardware text mode color constants. */
-enum vga_color {
+enum vga_color
+{
 	VGA_COLOR_BLACK = 0,
 	VGA_COLOR_BLUE = 1,
 	VGA_COLOR_GREEN = 2,
@@ -31,93 +32,114 @@ enum vga_color {
 	VGA_COLOR_LIGHT_BROWN = 14,
 	VGA_COLOR_WHITE = 15,
 };
- 
-static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) 
+
+static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)
 {
 	return fg | bg << 4;
 }
- 
-static inline uint16_t vga_entry(unsigned char uc, uint8_t color) 
+
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
 {
-	return (uint16_t) uc | (uint16_t) color << 8;
+	return (uint16_t)uc | (uint16_t)color << 8;
 }
- 
-size_t strlen(const char* str) 
+
+size_t strlen(const char *str)
 {
 	size_t len = 0;
 	while (str[len])
 		len++;
 	return len;
 }
- 
+
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
- 
+
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
-uint16_t* terminal_buffer;
- 
-void terminal_initialize(void) 
+uint16_t *terminal_buffer;
+
+void terminal_initialize(void)
 {
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0xB8000;
-	for (size_t y = 0; y < VGA_HEIGHT; y++) {
-		for (size_t x = 0; x < VGA_WIDTH; x++) {
+	terminal_buffer = (uint16_t *)0xB8000;
+	for (size_t y = 0; y < VGA_HEIGHT; y++)
+	{
+		for (size_t x = 0; x < VGA_WIDTH; x++)
+		{
 			const size_t index = y * VGA_WIDTH + x;
 			terminal_buffer[index] = vga_entry(' ', terminal_color);
 		}
 	}
 }
- 
-void terminal_setcolor(uint8_t color) 
+
+void terminal_setcolor(uint8_t color)
 {
 	terminal_color = color;
 }
- 
-void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
+
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 {
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_entry(c, color);
 }
- 
-void terminal_putchar(char c) 
-{
-    if(c == '\n'){
-        terminal_column = 0;
-        terminal_row++;
-    }
-    
-    else{
 
-	    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-	    if (++terminal_column == VGA_WIDTH) {
-	    	terminal_column = 0;
-		    if (++terminal_row == VGA_HEIGHT)
-			    terminal_row = 0;
-	    }
-    }
+void terminal_scrolling()
+{
+		for (size_t x = 0; x < VGA_WIDTH; x++)
+		{
+			const size_t index = 0 * VGA_WIDTH + x;
+			terminal_buffer[index] = vga_entry(terminal_buffer[1 * VGA_WIDTH + x], terminal_color);
+		}
+
+		for (size_t x = 0; x < VGA_WIDTH; x++)
+		{
+			const size_t index = 1 * VGA_WIDTH + x;
+			terminal_buffer[index] = vga_entry(terminal_buffer[2 * VGA_WIDTH + x], terminal_color);
+		}
 
 }
- 
-void terminal_write(const char* data, size_t size) 
+void terminal_putchar(char c)
+{
+	if (c == '\n')
+	{
+		terminal_column = 0;
+		terminal_row++;
+	}
+
+	else
+	{
+
+		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+		if (++terminal_column == VGA_WIDTH)
+		{
+			terminal_column = 0;
+		}
+		if (terminal_row >= 2)
+		{
+			terminal_scrolling();
+		}
+	}
+}
+
+void terminal_write(const char *data, size_t size)
 {
 	for (size_t i = 0; i < size; i++)
 		terminal_putchar(data[i]);
 }
- 
-void terminal_writestring(const char* data) 
+
+void terminal_writestring(const char *data)
 {
 	terminal_write(data, strlen(data));
 }
- 
-void kernel_main(void) 
+
+void kernel_main(void)
 {
 	/* Initialize terminal interface */
 	terminal_initialize();
- 
+
 	/* Newline support is left as an exercise. */
-	terminal_writestring("Hello, kernel World!\nIt's my first kernel.\n");
+	terminal_writestring("Hello, kernel World!\nIt's my first kernel.\n123\nabb");
 }
